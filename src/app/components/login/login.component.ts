4da7a6ee431a170c69ee75421c63a39921ade85e
SebSeitz <sebastian_seitz13@web.de>
2023-05-31 12:51:31 +0200

    old version without http interceptor

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -11,18 +12,19 @@ export class LoginComponent implements OnInit {
   username: string = '';
   password: string = '';
 
-  constructor(private http: HttpClient, private as:AuthService) { }
+  constructor(private as: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   async login() {
-
     try {
       let resp = await this.as.loginWithUsernameAndPassword(this.username, this.password);
       console.log(resp);
+      this.router.navigateByUrl('/todos');
     } catch (e) {
       console.error(e);
+      alert('Login fehlgeschlagen');
     }
   }
 
